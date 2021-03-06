cd8be24d5e86b4a1ddb3e9da2b0338a9eb1a5f9e
BoiRaigy <stnerik7900@gmail.com>
2021-03-07 00:09:04 +0100

    -19

--- NoNameButtonGame/LevelSystem/LevelContainer/Level19.cs
+++ NoNameButtonGame/LevelSystem/LevelContainer/Level19.cs
@@ -20,23 +20,31 @@ namespace NoNameButtonGame.LevelSystem.LevelContainer
     class Level19 : SampleLevel
     {
 
-        AwesomeButton button;
+        HoldButton button;
         Cursor cursor;
-        TextBuilder[] Infos;
-        Laserwall wall;
+        LockButton lockbutton;
+        Random rand;
+        TextBuilder GUN;
+        List<Tuple<Laserwall, Vector2>> shots;
         public Level19(int defaultWidth, int defaultHeight, Vector2 window, Random rand) : base(defaultWidth, defaultHeight, window, rand) {
-            Name = "Level 4 - Bugs? No its a Feature!";
-            button = new AwesomeButton(new Vector2(-256, -0), new Vector2(128, 64), Globals.Content.GetTHBox("awesomebutton"));
-            button.Click += BtnEvent;
+            Name = "Level 19 - Hold Click Repeat!";
+            this.rand = rand;
+            GUN = new TextBuilder("AGUN", new Vector2(-256, 0), new Vector2(16, 16), null, 0);
+            button = new HoldButton(new Vector2(-64, -32), new Vector2(128, 64), Globals.Content.GetTHBox("emptybutton"));
+            button.EndHoldTime = 30000;
+            button.Click += EmptyBtnEvent;
             cursor = new Cursor(new Vector2(0, 0), new Vector2(7, 10), Globals.Content.GetTHBox("cursor"));
-            Infos = new TextBuilder[2];
-            Infos[0] = new TextBuilder("Thin walls can be penetrated!", new Vector2(80, -132), new Vector2(8, 8), null, 0);
-            Infos[1] = new TextBuilder("Just move fast enough!", new Vector2(80, -100), new Vector2(8, 8), null, 0);
-            wall = new Laserwall(new Vector2(-40, -300), new Vector2(24, 1024), Globals.Content.GetTHBox("zonenew"));
-            wall.Enter += WallEvent;
+            
+            lockbutton = new LockButton(new Vector2(-192, -32), new Vector2(128, 64), Globals.Content.GetTHBox("awesomebutton"), true);
+            lockbutton.Click += BtnEvent;
+            shots = new List<Tuple<Laserwall, Vector2>>();
+            OldMPos = new Vector2(0, 0);
         }
 
 
+        private void EmptyBtnEvent(object sender, EventArgs e) {
+            lockbutton.Locked = !lockbutton.Locked;
+        }
 
         private void BtnEvent(object sender, EventArgs e) {
             CallFinish(sender, e);
@@ -46,23 +54,64 @@ namespace NoNameButtonGame.LevelSystem.LevelContainer
         }
         public override void Draw(SpriteBatch sp) {
             button.Draw(sp);
-            for (int i = 0; i < Infos.Length; i++) {
-                Infos[i].Draw(sp);
-            }
-            wall.Draw(sp);
+            
+            lockbutton.Draw(sp);
             cursor.Draw(sp);
+            GUN.Draw(sp);
+            for (int i = 0; i < shots.Count; i++) {
+                shots[i].Item1.Draw(sp);
+            }
         }
-
+        float GT;
+        float MGT;
+        float ShotTime = 350;
+        float TravelSpeed = 5;
+        float UpdateSpeed = 2;
+        float MaxUpdateSpeed = 10;
+        float MinUpdateSpeed = 10;
+        Vector2 OldMPos;
+        List<int> removeItem = new List<int>();
         public override void Update(GameTime gt) {
+            
             cursor.Update(gt);
             base.Update(gt);
-            for (int i = 0; i < Infos.Length; i++) {
-                Infos[i].Update(gt);
-            }
-
+            GUN.Update(gt);
             cursor.Position = MousePos - cursor.Size / 2;
             button.Update(gt, cursor.Hitbox[0]);
-            wall.Update(gt, cursor.Hitbox[0]);
+            lockbutton.Update(gt, cursor.Hitbox[0]);
+            MGT += (float)gt.ElapsedGameTime.TotalMilliseconds;
+            while (MGT > UpdateSpeed) {
+                MGT -= UpdateSpeed;
+                for (int i = 0; i < shots.Count; i++) {
+                    shots[i].Item1.Move(shots[i].Item2 * TravelSpeed);
+                }
+                GT += (float)gt.ElapsedGameTime.TotalMilliseconds;
+                while (GT > ShotTime) {
+                    GT -= ShotTime;
+                    Vector2 Dir = cursor.Hitbox[0].Center.ToVector2() - GUN.rec.Center.ToVector2();
+                    shots.Add(new Tuple<Laserwall, Vector2>(new Laserwall(GUN.Position, new Vector2(16, 8), Globals.Content.GetTHBox("zonenew")), Dir / Dir.Length()));
+                    shots[shots.Count - 1].Item1.Enter += CallFail;
+                }
+            }
+            removeItem.Clear();
+            for (int i = 0; i < shots.Count; i++) {
+                shots[i].Item1.Update(gt, cursor.Hitbox[0]);
+                if (!shots[i].Item1.rec.Intersects(CamRec)) {
+                    removeItem.Add(i);
+                }
+            }
+            for (int i = 0; i < removeItem.Count; i++) {
+                try {
+                    shots.RemoveAt(removeItem[i]);
+                } catch { }
+            }
+            if (MousePos != OldMPos) {
+                UpdateSpeed -= Vector2.Distance(MousePos, OldMPos) * 10;
+                if (UpdateSpeed < MinUpdateSpeed)
+                    UpdateSpeed = MinUpdateSpeed;
+            } else {
+                UpdateSpeed = MaxUpdateSpeed;
+            }
         }
     }
 }
